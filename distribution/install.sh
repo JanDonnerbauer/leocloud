a188e97cd467c53999bfb570ceb81ba5d84b9b1f
Jan Donnerbauer <donnerbauerjan@gmail.com>
2021-11-13 13:26:52 +0100

    Update install.sh

--- distribution/install.sh
+++ distribution/install.sh
@@ -5,7 +5,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # GITHUB_USER=$(echo $1 | tr '[:upper:]' '[:lower:]')"
-GITHUB_USER=jandonnerbauer
+# GITHUB_USER=jandonnerbauer
+GITHUB_USER=$1
 ACCESS_TOKEN=$2
 IMAGE=$3
 SERVER_USER=$4
